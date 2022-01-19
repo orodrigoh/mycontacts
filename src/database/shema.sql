da135ee513a62cce88999b233de75c473b031b32
Rodrigo <roodrigoooh@gmail.com>
2022-01-18 21:42:03 -0300

    ready

---
+++ src/database/shema.sql
@@ -0,0 +1,24 @@
+-- docker exec it pg bash
+-- psql -U root
+-- \l listar base de dados
+-- \c mycontacts - Conecta na base de dados
+-- \dt listar tabelas
+
+CREATE DATABASE mycontacts;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR ,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
+
